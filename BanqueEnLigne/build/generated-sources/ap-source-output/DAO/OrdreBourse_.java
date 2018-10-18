04e70d585955b6502b491e33ef9e3316fc8cf8a5
Julien Berthelot <julien.berthelot@etu.univ-poitiers.fr>
2018-10-18 11:43:40 +0200

    modification bug

--- BanqueEnLigne/build/generated-sources/ap-source-output/DAO/OrdreBourse_.java
+++ BanqueEnLigne/build/generated-sources/ap-source-output/DAO/OrdreBourse_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-10-18T11:33:02")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-10-18T11:39:41")
 @StaticMetamodel(OrdreBourse.class)
 public class OrdreBourse_ { 
 
